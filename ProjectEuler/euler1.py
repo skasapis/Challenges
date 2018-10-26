4e03750f61960403f56052f29f0a70968ddb6ecc
Spyros Kasapis <44474826+skasapis@users.noreply.github.com>
2018-10-26 13:28:05 -0400

    Update euler1.py
    Proposed changes to the code and commenting

--- ProjectEuler/euler1.py
+++ ProjectEuler/euler1.py
@@ -1,6 +1,7 @@
-sum = 0
-for i in range(0,1000):
-    if (i%3==0) or (i%5==0):
-        sum+=i
-
-print(sum)
+sum = 0 #initialization
+for i in range(0,1000): #would be 1:1000 (notice python starts count from 0)
+    if (i%3==0) or (i%5==0): #% is the matlab mod function
+        sum = sum + i #sum+=i is a different way to write sum = sum+1
+    #notice there in no "end" in python
+#notice there in no "end" in python
+print(sum) #print outcome
