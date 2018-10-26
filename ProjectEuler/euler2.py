9b19345f4b17bd13e688d18a351a5ac7bd327c8e
Spyros Kasapis <44474826+skasapis@users.noreply.github.com>
2018-10-26 13:42:43 -0400

    Update euler2.py
    This change will give the same answer while having less lines of code.

--- ProjectEuler/euler2.py
+++ ProjectEuler/euler2.py
@@ -1,10 +1,7 @@
 prev, cur = 0,1
 sum = 0
-while True:
+while cur <=4000000:
     prev, cur = cur, prev+cur
-    if cur >= 4000000:
-        break
     if cur % 2 == 0:
          sum += cur
-
 print(sum)
